89854a119c6fdbf4202ecaee7abfab7a376bddce
zhongyu22 <136547513+zhongyu22@users.noreply.github.com>
2023-06-14 19:59:34 +1000

    Environment settings

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("Server is running");
+})
+
+app.listen(3000, function() {
+    console.log("");
+})
